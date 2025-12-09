2f46d64e52952761876ea4f84b7eb308b1646e9c
JordyRomeroa <jromeroa5@est.ups.edu.ec>
2025-12-08 23:51:46 -0500

    configuracion Firebase

---
+++ ProyectoIntegradorWeb/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDUsfcQfSUjRa2Gndx3YwOOCFje5siedEA",
+  authDomain: "project-8814620132973596075.firebaseapp.com",
+  projectId: "project-8814620132973596075",
+  storageBucket: "project-8814620132973596075.firebasestorage.app",
+  messagingSenderId: "473993498468",
+  appId: "1:473993498468:web:4e27c300dbed2a24a57471"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
